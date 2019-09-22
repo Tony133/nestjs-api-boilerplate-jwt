ee98e59b2cf6efdb6e392bc8d3c6d20399ded02f
Tony133 <a.tripodi133@gmail.com>
2019-09-22 17:16:12 +0200

    Add example resource protected with token JWT

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get, Res, HttpStatus } from '@nestjs/common';
+import { Controller, Get, Res, HttpStatus, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
 import { Response } from 'express';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller()
 export class AppController {
@@ -10,6 +11,11 @@ export class AppController {
   @Get()
   getHello(@Res() res: Response) {
     return res.status(HttpStatus.OK).json(this.appService.getHello());
+  }
 
+  @UseGuards(AuthGuard('jwt'))
+  @Get('api/secure')
+  getProtectedResource(@Res() res: Response) {
+    return res.status(HttpStatus.OK).json(this.appService.getSecureResource());
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -5,4 +5,8 @@ export class AppService {
   getHello() {
     return { message: 'This is a simple example of item returned by your APIs.' };
   }
+
+  getSecureResource() {
+    return { message: 'Access to protected resources granted! This protected resource is displayed when the token is successfully provided.'}
+  }
 }
