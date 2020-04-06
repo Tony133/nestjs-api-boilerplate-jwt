ceabe51e00810dd21e8d1bed6c052c20e505f73d
Tony133 <a.tripodi133@gmail.com>
2020-04-06 22:22:03 +0200

    Enable Cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
