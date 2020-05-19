626fdadbfb35754ee0ca9a2c4a4933ee59ab0442
Tony133 <a.tripodi133@gmail.com>
2020-05-19 22:44:20 +0200

    Update login controller

--- src/login/login.controller.ts
+++ src/login/login.controller.ts
@@ -1,14 +1,13 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { LoginService } from './login.service';
 import { User } from '../entity/User';
-import { LoginDTO } from './login.dto';
 
 @Controller('api/auth/login')
 export class LoginController {
   constructor(private readonly loginService: LoginService) {}
 
   @Post()
-  async login(@Body() user: User, login: LoginDTO): Promise<any> {
+  async login(@Body() user: User): Promise<any> {
     return this.loginService.login(user);
   }
 }
