faba7460b3b4956e9d2cfb9af79e4b6ad9d9dddf
Tony133 <a.tripodi133@gmail.com>
2021-11-08 22:24:11 +0100

    Update settings template mail

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,15 @@
-import { Module } from "@nestjs/common";
-import { ConfigModule } from "@nestjs/config";
-import { TypeOrmModule } from "@nestjs/typeorm";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
-import { LoginModule } from "./login/login.module";
-import { RegisterModule } from "./register/register.module";
-import { UsersModule } from "./users/users.module";
-import { ForgotPasswordModule } from "./forgot-password/forgot-password.module";
-import { ChangePasswordModule } from "./change-password/change-password.module";
-import { MailerModule } from "@nestjs-modules/mailer";
-import { HandlebarsAdapter } from "@nestjs-modules/mailer/dist/adapters/handlebars.adapter";
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { LoginModule } from './login/login.module';
+import { RegisterModule } from './register/register.module';
+import { UsersModule } from './users/users.module';
+import { ForgotPasswordModule } from './forgot-password/forgot-password.module';
+import { ChangePasswordModule } from './change-password/change-password.module';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
 
 @Module({
   imports: [
@@ -34,7 +34,7 @@ import { HandlebarsAdapter } from "@nestjs-modules/mailer/dist/adapters/handleba
           from: '"nest-modules" <modules@nestjs.com>',
         },
         template: {
-          dir: process.cwd() + "/templates/emails",
+          dir: process.cwd() + './templates/emails',
           adapter: new HandlebarsAdapter(), // or new PugAdapter() or new EjsAdapter()
           options: {
             strict: true,
