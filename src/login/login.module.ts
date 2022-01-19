126dcbfd652dea680decd1b0794708362809aaff
Tony133 <a.tripodi133@gmail.com>
2022-01-19 17:43:47 +0100

    Update login.module.ts

--- src/login/login.module.ts
+++ src/login/login.module.ts
@@ -1,24 +1,28 @@
-import { Module } from "@nestjs/common";
-import { LoginService } from "./login.service";
-import { LoginController } from "./login.controller";
-import { TypeOrmModule } from "@nestjs/typeorm";
-import { Users } from "../users/entities/users.entity";
-import { JwtModule } from "@nestjs/jwt";
-import { UsersService } from "../users/users.service";
-import { PassportModule } from "@nestjs/passport";
-import { JwtStrategy } from "./strategies/jwt.strategy";
-import { ConfigModule } from '@nestjs/config';
+import { Module } from '@nestjs/common';
+import { LoginService } from './login.service';
+import { LoginController } from './login.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Users } from '../users/entities/users.entity';
+import { JwtModule } from '@nestjs/jwt';
+import { UsersService } from '../users/users.service';
+import { PassportModule } from '@nestjs/passport';
+import { JwtStrategy } from './strategies/jwt.strategy';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     TypeOrmModule.forFeature([Users]),
-    PassportModule.register({ defaultStrategy: "jwt", session: false }),
-    JwtModule.register({
-      secret: process.env.SECRET_KEY_JWT,
-      signOptions: {
-        expiresIn: 3600,
-      },
+    PassportModule.register({ defaultStrategy: 'jwt', session: false }),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('SECRET_KEY_JWT'),
+        signOptions: {
+          expiresIn: 3600,
+        },
+      }),
+      inject: [ConfigService],
     }),
   ],
   providers: [LoginService, UsersService, JwtStrategy],
