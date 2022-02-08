c6823a3a212a24248fa8ae5c46f7855a1e434d8d
Tony133 <a.tripodi133@gmail.com>
2022-02-08 21:51:02 +0100

    Update for database configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -13,7 +13,28 @@ import { MailerModule } from './mailer/mailer.module';
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    TypeOrmModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql', // type of our database
+        host: configService.get<string>('DB_MYSQL_HOST'), // database host
+        port: configService.get<number>('DB_MYSQL_PORT'), // database host
+        username: configService.get<string>('DB_MYSQL_USER'), // user
+        password: configService.get<string>('DB_MYSQL_PASSWORD'), // password
+        database: configService.get<string>('DB_MYSQL_DATABASE'), // name of our database,
+        autoLoadEntities: true, // models will be loaded automatically (you don't have to explicitly specify the entities: [] array)
+        synchronize: true, // your entities will be synced with the database (ORM will map entity definitions to corresponding SQL tabled), every time you run the application (recommended: disable in the production)
+        logging: false,
+        entities: ['dist/**/*.entity.js'],
+        migration: ['dist/migrations/**/*.js'],
+        subscribers: ['dist/subscribers/**/*.js'],
+        cli: {
+          migrationsDir: 'src/migrations',
+          subscribersDir: 'src/subscribers',
+        },
+      }),
+    }),
     LoginModule,
     RegisterModule,
     UsersModule,
