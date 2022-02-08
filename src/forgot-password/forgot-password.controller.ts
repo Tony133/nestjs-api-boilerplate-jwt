e53744b8c04a5364e203923a82d6ef5ad66dc646
Tony <Tony133@users.noreply.github.com>
2022-02-08 22:47:54 +0100

    Add open api swagger (#110)
    * Add open api swagger

--- src/forgot-password/forgot-password.controller.ts
+++ src/forgot-password/forgot-password.controller.ts
@@ -1,26 +1,28 @@
-import { Controller, Post, Body, Res, HttpStatus } from "@nestjs/common";
-import { ForgotPasswordService } from "../forgot-password/forgot-password.service";
-import { ForgotPasswordDto } from "./dto/forgot-password.dto";
+import { Controller, Post, Body, Res, HttpStatus } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { ForgotPasswordService } from '../forgot-password/forgot-password.service';
+import { ForgotPasswordDto } from './dto/forgot-password.dto';
 
-@Controller("auth/forgot-password")
+@ApiTags('auth')
+@Controller('auth/forgot-password')
 export class ForgotPasswordController {
   constructor(private readonly forgotPasswordService: ForgotPasswordService) {}
 
   @Post()
   public async forgotPassword(
     @Res() res,
-    @Body() forgotPasswordDto: ForgotPasswordDto
+    @Body() forgotPasswordDto: ForgotPasswordDto,
   ): Promise<any> {
     try {
       await this.forgotPasswordService.forgotPassword(forgotPasswordDto);
 
       return res.status(HttpStatus.OK).json({
-        message: "Request Reset Password Successfully!",
+        message: 'Request Reset Password Successfully!',
         status: 200,
       });
     } catch (err) {
       return res.status(HttpStatus.BAD_REQUEST).json({
-        message: "Error: Forgot password failed!",
+        message: 'Error: Forgot password failed!',
         status: 400,
       });
     }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,23 +8,28 @@ import {
   UseGuards,
   HttpStatus,
   NotFoundException,
-} from "@nestjs/common";
-import { UsersService } from "./users.service";
-import { AuthGuard } from "@nestjs/passport";
-import { UserProfileDto } from "./dto/user-profile.dto";
-import { IUsers } from "./interfaces/users.interface";
+} from '@nestjs/common';
+import { UsersService } from './users.service';
+import { AuthGuard } from '@nestjs/passport';
+import { UserProfileDto } from './dto/user-profile.dto';
+import { IUsers } from './interfaces/users.interface';
+import { ApiTags } from '@nestjs/swagger';
 
-@UseGuards(AuthGuard("jwt"))
-@Controller("users")
+@ApiTags('users')
+@UseGuards(AuthGuard('jwt'))
+@Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
-  @Get("/:userId/profile")
-  public async getUser(@Res() res, @Param("userId") userId: string ): Promise<IUsers> {
+  @Get('/:userId/profile')
+  public async getUser(
+    @Res() res,
+    @Param('userId') userId: string,
+  ): Promise<IUsers> {
     const user = await this.usersService.findById(userId);
 
     if (!user) {
-      throw new NotFoundException("User does not exist!");
+      throw new NotFoundException('User does not exist!');
     }
 
     return res.status(HttpStatus.OK).json({
@@ -33,22 +38,22 @@ export class UsersController {
     });
   }
 
-  @Put("/:userId/profile")
+  @Put('/:userId/profile')
   public async updateProfileUser(
     @Res() res,
-    @Param('userId') userId: string, 
-    @Body() userProfileDto: UserProfileDto
+    @Param('userId') userId: string,
+    @Body() userProfileDto: UserProfileDto,
   ): Promise<any> {
     try {
       await this.usersService.updateProfileUser(userId, userProfileDto);
 
       return res.status(HttpStatus.OK).json({
-        message: "User Updated successfully!",
+        message: 'User Updated successfully!',
         status: 200,
       });
     } catch (err) {
       return res.status(HttpStatus.BAD_REQUEST).json({
-        message: "Error: User not updated!",
+        message: 'Error: User not updated!',
         status: 400,
       });
     }
