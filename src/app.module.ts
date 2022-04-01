98d421d37d2db872378757b3e674d981909bed72
Tony133 <a.tripodi133@gmail.com>
2022-04-01 17:54:48 +0200

    Add rate limiting

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,12 +9,21 @@ import { UsersModule } from './users/users.module';
 import { ForgotPasswordModule } from './forgot-password/forgot-password.module';
 import { ChangePasswordModule } from './change-password/change-password.module';
 import { MailerModule } from './mailer/mailer.module';
+import { ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: ['.env', '.env.dev', '.env.stage', '.env.prod'],
+    }),
+    ThrottlerModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        ttl: config.get<number>('THROTTLE_TTL'),
+        limit: config.get<number>('THROTTLE_LIMIT'),
+      }),
     }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
