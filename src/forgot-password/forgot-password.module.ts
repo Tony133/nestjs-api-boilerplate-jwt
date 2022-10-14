fa1254bc712b9f4d2d54b80d6806f5d6e74abd47
Tony133 <a.tripodi133@gmail.com>
2022-10-14 18:17:42 +0200

    chore: update forgot password

--- src/forgot-password/forgot-password.module.ts
+++ src/forgot-password/forgot-password.module.ts
@@ -5,9 +5,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Users } from '../users/entities/users.entity';
 import { UsersService } from '../users/users.service';
 import { MailerModule } from '../mailer/mailer.module';
+import { UtilsModule } from '../utils/utils.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Users]), MailerModule],
+  imports: [TypeOrmModule.forFeature([Users]), MailerModule, UtilsModule],
   providers: [ForgotPasswordService, UsersService],
   controllers: [ForgotPasswordController],
 })
---
+++ src/utils/utils.module.ts
@@ -0,0 +1,8 @@
+import { Module } from '@nestjs/common';
+import { UtilsService } from './utils.service';
+
+@Module({
+  providers: [UtilsService],
+  exports: [UtilsService],
+})
+export class UtilsModule {}
