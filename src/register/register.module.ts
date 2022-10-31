e42c1e19ecae27eaeab8d20e9c400da438d7fa7a
Tony133 <a.tripodi133@gmail.com>
2022-10-31 18:24:27 +0100

    chore: update import in register module

--- src/register/register.module.ts
+++ src/register/register.module.ts
@@ -4,7 +4,7 @@ import { RegisterController } from './register.controller';
 import { RegisterService } from './register.service';
 import { UsersService } from '../users/users.service';
 import { Users } from '../users/entities/users.entity';
-import { MailerModule } from 'src/mailer/mailer.module';
+import { MailerModule } from '../mailer/mailer.module';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Users]), MailerModule],
