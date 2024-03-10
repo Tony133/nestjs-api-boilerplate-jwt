f8e21d521719270abd6d7c7990edb5935834492e
Tony133 <a.tripodi133@gmail.com>
2024-03-10 16:50:56 +0100

    fix: updated register.module.ts

--- src/iam/register/register.module.ts
+++ src/iam/register/register.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { BcryptService } from '@/shared/hashing/bcrypt.service';
-import { HashingService } from '@/shared/hashing/hashing.service';
-import { MailerModule } from '@/shared/mailer/mailer.module';
-import { Users } from '@/users/models/users.model';
-import { UsersService } from '@/users/users.service';
+import { BcryptService } from '../../shared/hashing/bcrypt.service';
+import { HashingService } from '../../shared/hashing/hashing.service';
+import { MailerModule } from '../../shared/mailer/mailer.module';
+import { Users } from '../../users/models/users.model';
+import { UsersService } from '../../users/users.service';
 import { RegisterController } from './register.controller';
 import { RegisterService } from './register.service';
-import { provideUsersRepository } from '@/users/repositories/users.repository.provider';
+import { provideUsersRepository } from '../../users/repositories/users.repository.provider';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Users]), MailerModule],
