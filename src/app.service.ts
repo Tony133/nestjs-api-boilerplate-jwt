337e79b94142d6e069238571c191275fb262a63b
Tony133 <a.tripodi133@gmail.com>
2024-06-15 19:05:44 +0200

    fix: update app.service.ts

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,13 +2,13 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello() {
+  getHello(): { message: string } {
     return {
       message: 'This is a simple example of item returned by your APIs.',
     };
   }
 
-  getSecureResource() {
+  getSecureResource(): { message: string } {
     return {
       message:
         'Access to protected resources granted! This protected resource is displayed when the token is successfully provided.',
