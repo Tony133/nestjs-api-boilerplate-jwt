0c40f4b0e408c6cec8ad72622dfd1b4c283ae26d
Tony133 <a.tripodi133@gmail.com>
2024-06-30 16:53:53 +0200

    chore: update eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -4,19 +4,18 @@ module.exports = {
     project: 'tsconfig.json',
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: ['@typescript-eslint'],
   extends: [
-    'plugin:@typescript-eslint/eslint-recommended',
+    'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'prettier',
   ],
-  root: true,
   env: {
     node: true,
     jest: true,
   },
   rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/naming-convention': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
   },
