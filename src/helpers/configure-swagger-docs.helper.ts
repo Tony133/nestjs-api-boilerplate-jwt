fbdb5a2298757984dd3bf21f8f7b757e539bb3c7
Antonio Tripodi <Tony133@users.noreply.github.com>
2025-06-22 10:59:43 +0200

    chore: update configure-swagger-docs.helper.ts

--- src/helpers/configure-swagger-docs.helper.ts
+++ src/helpers/configure-swagger-docs.helper.ts
@@ -23,32 +23,6 @@ export function configureSwaggerDocs(app: INestApplication) {
     const documentFactory = () => SwaggerModule.createDocument(app, config);
     SwaggerModule.setup('/docs', app, documentFactory, {
       explorer: true,
-      customfavIcon: '/favicon.ico',
-      customSiteTitle: 'API Documentation',
-      customCss: `
-        .swagger-ui svg {
-          content: url('/favicon.ico');
-        }
-        // .swagger-ui .topbar {
-        // }
-        // .swagger-ui .scheme-container { display: block; }
-        // .swagger-ui .information-container { display: none; }
-        // .swagger-ui .opblock-tag { background-color: #f8f9fa; }
-        // .swagger-ui .opblock-tag:hover { background-color: #e9ecef; }
-        // .swagger-ui .opblock-summary { background-color: #f1f3f5; }
-        // .swagger-ui .opblock-summary:hover { background-color: #e9ecef; }
-        // .swagger-ui .opblock-summary-method { color: #495057; }
-        // .swagger-ui .opblock-summary-path { color: #6c757d; }
-        // .swagger-ui .opblock-description { color: #343a40; }
-        // .swagger-ui .opblock-body { background-color: #ffffff; }
-        // .swagger-ui .opblock-body pre { background-color: #f8f9fa; }
-        // .swagger-ui .opblock-body code { color: #212529; }
-        // .swagger-ui .opblock-body .parameters { border: 1px solid #ced4da; }
-        // .swagger-ui .opblock-body .responses { border: 1px solid #ced4da; }
-        // .swagger-ui .opblock-body .response { background-color: #ffffff; }
-        // .swagger-ui .opblock-body .response pre { background-color: #f8f9fa; }
-        // .swagger-ui .opblock-body .response code { color: #212529; }
-      `,
       swaggerOptions: {
         filter: true,
         showRequestDuration: true,
